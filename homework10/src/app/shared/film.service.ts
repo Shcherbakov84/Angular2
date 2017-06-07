e926f35bcc3348de4060cbcb4b76a019b3b859cb
Alexander Shcherbakov <oosherbakov@gmail.com>
2017-06-07 09:19:00 +0300

    убрал в сервисе  import  { OnInit }

--- homework10/src/app/shared/film.service.ts
+++ homework10/src/app/shared/film.service.ts
@@ -1,18 +1,15 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class FilmService implements OnInit {
+export class FilmService {
   url: string = 'http://www.omdbapi.com';
   apiKey: string = '520bbe17';
   page: number = 1;
   
   constructor(private http: Http) { }
 
-  ngOnInit() {
-  }
-  
   private extract(res: Response) {
     let body = res.json(); 
     body = body.Search || body;
